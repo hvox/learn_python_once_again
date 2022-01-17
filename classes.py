f2b4965d60fd274926d51b965b16ba99f394f3d3
Uladzislau Khamkou <kham@tuta.io>
2022-01-17 18:12:00 +0300

    classes: add Dataclass way to create classes

--- classes.py
+++ classes.py
@@ -3,3 +3,9 @@ NamedTuple = lambda flds: namedtuple('Σ', flds)
 class Point(NamedTuple('x y')):
     pass
 print(Point(1, 2))
+
+from dataclasses import make_dataclass
+Dataclass = lambda flds: make_dataclass('ξ', flds.split())
+class Point(Dataclass('x y')):
+    pass
+print(Point(1, 2))
