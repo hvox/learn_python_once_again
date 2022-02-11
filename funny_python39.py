256e425db8b6533aad98e4a813eecfc7a228abbd
Uladzislau Khamkou <kham@tuta.io>
2022-02-11 22:03:22 +0300

    are you making all this up as you go along?

---
+++ funny_python39.py
@@ -0,0 +1,6 @@
+d1 = {'a' : 1, 'b' : 3}
+d2 = {'b' : 2, 'c' : 4}
+assert d1 | d2 != d2 | d1
+assert type(list[list]) != type(list)
+print("Hello".removeprefix("hi").removesuffix("o"))
+print(__import__('math').ulp(0))
