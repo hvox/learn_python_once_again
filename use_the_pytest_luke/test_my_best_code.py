723717ddc73cc245f785661545e0f8883a7ba990
Uladzislau Khamkou <kham@tuta.io>
2022-05-05 10:52:05 +0300

    use fixtures

--- use_the_pytest_luke/test_my_best_code.py
+++ use_the_pytest_luke/test_my_best_code.py
@@ -2,11 +2,20 @@ import pytest
 
 what_is_the_answer, is_the_answer = lambda x=None: x or 42, lambda x: x == 42
 everything = set(range(1, 100))
-everything |= set("I like everything about pytest except its speed.")
+everything |= set("I like everything about pytest except its speed")
+
+
+@pytest.fixture
+def the_data():
+    return "I am the data you don't want to initialize"
 
 
 def test_the_answer():
     assert what_is_the_answer() == 42
+
+
+def test_fixtures():
+    assert what_is_the_answer(the_data)
 
 
 @pytest.mark.parametrize("something", everything)
