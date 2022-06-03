fb4ba8801526928eaf95eb04fd1d0d6f27406bca
Uladzislau Khamkou <kham@tuta.io>
2022-06-03 11:20:05 +0300

    support expression evaluation in REPL of sandbox

--- concepts/sandbox.py
+++ concepts/sandbox.py
@@ -10,11 +10,24 @@ def exec(code, scope=None, access=None):
     return scope
 
 
+def eval(code, scope=None, access=None):
+    scope = {} if scope is None else scope
+    access = {"__builtins__": {}, "print": print} | (access or {})
+    return __builtins__.eval(code, access, scope)
+
+
 if __name__ == "__main__":
-    if sys.argv[1:]:
+    if len(sys.argv) > 1:
         for path in sys.argv[1:]:
             exec(pathlib.Path(path).read_text())
     else:
         scope = {}
-        while src := input():
-            exec(src, scope)
+        while True:
+            src = input("> ")
+            try:
+                try:
+                    print(repr(eval(src, scope)))
+                except SyntaxError:
+                    exec(src, scope)
+            except Exception as e:
+                print(repr(e))
