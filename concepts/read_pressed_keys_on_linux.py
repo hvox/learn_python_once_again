406f83c0529a909e7ff6e335be71c58dc91e6514
Uladzislau Khamkou <kham@tuta.io>
2022-09-22 22:07:46 +0300

    keys on Linux: rewrite the thing

--- concepts/read_pressed_keys_on_linux.py
+++ concepts/read_pressed_keys_on_linux.py
@@ -2,100 +2,72 @@
 import contextlib
 import fcntl
 import os
+import queue
 import select
 import sys
 import termios
-
-CONTROL_CODES = {
-    b"\x7f": "backspace",
-    b"\x1b": "escape",
-    b"\x1b[3~": "delete",
-    b"\x1b[2~": "insert",
-    b"\x1b[A": "up",
-    b"\x1b[D": "left",
-    b"\x1b[B": "down",
-    b"\x1b[C": "right",
-    b"\x1b[E": "five",
-    b"\x1b[G": "five",
-    b"\x1b[H": "home",
-    b"\x1b[1~": "home",
-    b"\x1b[F": "end",
-    b"\x1b[4~": "end",
-    b"\x1b[5~": "pg_up",
-    b"\x1b[6~": "pg_down",
-    b"\x1bOP": "f1",
-    b"\x1b[[A": "f1",
-    b"\x1bOQ": "f2",
-    b"\x1b[[B": "f2",
-    b"\x1bOR": "f3",
-    b"\x1b[[C": "f3",
-    b"\x1bOS": "f4",
-    b"\x1b[[D": "f4",
-    b"\x1b[15~": "f5",
-    b"\x1b[[E": "f5",
-    b"\x1b[17~": "f6",
-    b"\x1b[18~": "f7",
-    b"\x1b[19~": "f8",
-    b"\x1b[20~": "f9",
-    b"\x1b[21~": "f10",
-    b"\x1b[23~": "f11",
-    b"\x1b[24~": "f12",
-}
+import threading
 
 
-def next_codepoint(bs: bytes) -> tuple[str, bytes]:
-    if not bs or (i := bin(bs[0])[2:].zfill(8).find("0")) not in (0, 2, 3, 4):
-        return "", bs
-    length = (0, 2, 3, 4).index(i) + 1
-    if length > len(bs):
-        return "", bs
-    return bs[:length].decode(), bs[length:]
-
-
-class NonblockingKeyReader:
+class KeyReader:
     def __init__(self):
-        assert sys.stdin.isatty()
-        self.buffer = b""
+        self.unix_mode = os.name != "nt"
+        assert self.unix_mode
+        self.pressed_keys = queue.Queue()
+        self._thread = None
 
-    def __enter__(self):
-        self.fd = sys.stdin.fileno()
-        self.tty_flags = termios.tcgetattr(self.fd)
-        self.fcntl_flags = fcntl.fcntl(self.fd, fcntl.F_GETFL)
-        cur_tty_flags = self.tty_flags.copy()
-        cur_tty_flags[3] &= ~termios.ICANON & ~termios.ECHO
-        termios.tcsetattr(self.fd, termios.TCSANOW, cur_tty_flags)
-        fcntl.fcntl(self.fd, fcntl.F_SETFL, self.fcntl_flags | os.O_NONBLOCK)
+    @property
+    def running(self):
+        return self._thread is not None
+
+    def start(self):
+        assert not self.running, "Can't start running key reader"
+        assert self.unix_mode
+        self._pipe = os.pipe2(os.O_NONBLOCK)
+        self._thread = threading.Thread(
+            target=read_keys, args=[self._pipe[0], self.pressed_keys]
+        )
+        self._thread.start()
+
+    def stop(self):
+        assert self.running, "Can't stop stopped key reader"
+        os.write(self._pipe[1], b"\x00")
+        self._thread.join()
+        self._thread = None
+        list(map(os.close, self._pipe))
+
+    def __enter__(self) -> "KeyReader":
+        self.start()
         return self
 
-    def read_key(self):
-        if len(self.buffer) < 5:
-            with contextlib.suppress(BlockingIOError):
-                self.buffer += os.read(self.fd, 5)
-        for size in range(len(self.buffer), 0, -1):
-            if (code := self.buffer[:size]) in CONTROL_CODES:
-                self.buffer = self.buffer[size:]
-                return CONTROL_CODES[code]
-        char, self.buffer = next_codepoint(self.buffer)
-        return char
-
-    def wait_key(self, timeout: float | None = None):
-        if key := self.read_key():
-            return key
-        select.select([sys.stdin], [], [], timeout)
-        return self.read_key()
-
-    def read_keys(self):
-        while key := self.read_key():
-            yield key
-
     def __exit__(self, *_):
-        termios.tcsetattr(self.fd, termios.TCSADRAIN, self.tty_flags)
-        fcntl.fcntl(self.fd, fcntl.F_SETFL, self.fcntl_flags)
+        self.stop()
+
+    def __del__(self):
+        assert not self.running, "You forgot to stop key reader"
 
 
-with NonblockingKeyReader() as key_reader:
+def read_keys(communication_fd: int, key_buffer: queue.Queue):
+    assert sys.stdin.isatty(), "Stdin does not look like TTY..."
+    fd = sys.stdin.fileno()
+    tty_flags = termios.tcgetattr(fd)
+    fcntl_flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    (cur_tty_flags := tty_flags.copy())[3] &= ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, cur_tty_flags)
+    fcntl.fcntl(fd, fcntl.F_SETFL, fcntl_flags | os.O_NONBLOCK)
+    rlist = [sys.stdin, communication_fd]
+    while select.select(rlist, [], [])[0] == [sys.stdin]:
+        chars = sys.stdin.read()
+        for char in chars:
+            key_buffer.put(char)
+    termios.tcsetattr(fd, termios.TCSADRAIN, tty_flags)
+    fcntl.fcntl(fd, fcntl.F_SETFL, fcntl_flags)
+
+
+with KeyReader() as key_reader:
     while True:
-        if key := key_reader.wait_key(timeout=5.0):
-            print("You pressed " + repr(key))
-        else:
-            print("You haven't pressed anything in last five seconds...")
+        key = key_reader.pressed_keys.get()
+        print("You pressed " + repr(key))
+        if key == "q":
+            break
+print(repr(input(">>> ")))
