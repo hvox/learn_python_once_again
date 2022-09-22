67ba402d9ee31aa650232f480f1773a087666604
Uladzislau Khamkou <kham@tuta.io>
2022-09-22 23:45:53 +0300

    keys on Linux: support input sequences

--- concepts/read_pressed_keys_on_linux.py
+++ concepts/read_pressed_keys_on_linux.py
@@ -46,6 +46,59 @@ class KeyReader:
         assert not self.running, "You forgot to stop key reader"
 
 
+INPUT_SEQUENCES = {
+    "[3~": "delete",
+    "[2~": "insert",
+    "[A": "up",
+    "[D": "left",
+    "[B": "down",
+    "[C": "right",
+    "[E": "five",
+    "[G": "five",
+    "[H": "home",
+    "[1~": "home",
+    "[F": "end",
+    "[4~": "end",
+    "[5~": "pageup",
+    "[6~": "pagedown",
+    "OP": "f1",
+    "[[A": "f1",
+    "OQ": "f2",
+    "[[B": "f2",
+    "OR": "f3",
+    "[[C": "f3",
+    "OS": "f4",
+    "[[D": "f4",
+    "[15~": "f5",
+    "[[E": "f5",
+    "[17~": "f6",
+    "[18~": "f7",
+    "[19~": "f8",
+    "[20~": "f9",
+    "[21~": "f10",
+    "[23~": "f11",
+    "[24~": "f12",
+}
+
+
+def parse_input_sequences(chars: str):
+    i = 0
+    while i < len(chars):
+        char = chars[i]
+        if char != "\x1b" or i + 1 == len(chars):
+            yield {"\x7f": "backspace", "\x1b": "escape"}.get(char, char)
+            i += 1
+            continue
+        for j in range(i + 5, i + 2, -1):
+            if chars[i + 1 : j] in INPUT_SEQUENCES:
+                yield INPUT_SEQUENCES[chars[i + 1 : j]]
+                i = j
+                break
+        else:
+            yield "alt+" + chars[i + 1]
+            i += 2
+
+
 def read_keys(communication_fd: int, key_buffer: queue.Queue):
     assert sys.stdin.isatty(), "Stdin does not look like TTY..."
     fd = sys.stdin.fileno()
@@ -56,9 +109,8 @@ def read_keys(communication_fd: int, key_buffer: queue.Queue):
     fcntl.fcntl(fd, fcntl.F_SETFL, fcntl_flags | os.O_NONBLOCK)
     rlist = [sys.stdin, communication_fd]
     while select.select(rlist, [], [])[0] == [sys.stdin]:
-        chars = sys.stdin.read()
-        for char in chars:
-            key_buffer.put(char)
+        for key in parse_input_sequences(sys.stdin.read()):
+            key_buffer.put(key)
     termios.tcsetattr(fd, termios.TCSADRAIN, tty_flags)
     fcntl.fcntl(fd, fcntl.F_SETFL, fcntl_flags)
 
