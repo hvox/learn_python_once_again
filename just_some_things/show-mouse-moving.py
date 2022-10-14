e85c0e0eca695f06c268a79205e4f3f9a102138f
Uladzislau Khamkou <kham@tuta.io>
2022-10-14 12:46:15 +0300

    add script to track mouse moving

---
+++ just_some_things/show-mouse-moving.py
@@ -0,0 +1,13 @@
+from math import atan2, tau
+from time import sleep
+
+from mouse import get_position
+
+x, y = get_position()
+while True:
+    x_prev, y_prev = x, y
+    x, y = get_position()
+    dx, dy = x - x_prev, y - y_prev
+    if dx or dy:
+        print("→↗↑↖←↙↓↘"[round(atan2(dy, dx) / (-tau / 8))])
+    sleep(0.02)
