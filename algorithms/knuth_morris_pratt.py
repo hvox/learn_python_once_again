aa8f3607cee7b6fed0f487199869ac90a96c4211
Uladzislau Khamkou <kham@tuta.io>
2022-10-28 23:59:16 +0300

    implement Knuth–Morris–Pratt algorithm

---
+++ algorithms/knuth_morris_pratt.py
@@ -0,0 +1,26 @@
+number_of_iterations = 0
+
+
+def prefix_function(s: str):
+    dp = [0] * len(s)
+    for i in range(1, len(s)):
+        j = dp[i - 1]
+        while j and s[j] != s[i]:
+            j = dp[j - 1]
+        dp[i] = j + (s[j] == s[i])
+    return dp
+
+
+def find(haystack: str, needle: str, default=None):
+    s = needle + haystack
+    dp = prefix_function(s)
+    for i in range(len(needle) * 2 - 1, len(s)):
+        if dp[i] >= len(needle):
+            return 1 + i - len(needle) * 2
+    return default
+
+
+while True:
+    s1 = input("haystack: ")
+    s2 = input("  needle: ")
+    print(find(s1, s2))
