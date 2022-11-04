cf498fd3a95fed95f126c41ecc1037dc17c01f1e
Uladzislau Khamkou <kham@tuta.io>
2022-11-04 20:33:00 +0300

    Add quicksort

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -6,6 +6,13 @@ def is_sorted(array: list):
     return not any(x > y for x, y in zip(array, array[1:]))
 
 
+def simplest_sort(array: list):
+    n = len(array)
+    for i, j in ((i, j) for i in range(n) for j in range(i, n)):
+        if array[i] > array[j]:
+            array[i], array[j] = array[j], array[i]
+
+
 def gitalev_sort(array: list):
     i = 0
     while i < len(array) - 1:
@@ -17,23 +24,35 @@ def gitalev_sort(array: list):
             i += 1
 
 
-def simplest_sort(array: list):
-    n = len(array)
-    for i, j in ((i, j) for i in range(n) for j in range(i, n)):
-        if array[i] > array[j]:
+def quicksort(array: list, left=0, right=~0):
+    left %= len(array)
+    right %= len(array)
+    if right - left < 2:
+        array[left:right+1] = list(sorted(array[left:right+1]))
+        return
+    pivot, i = array[left], left
+    for j in range(i + 1, right + 1):
+        if array[j] <= pivot:
+            i += 1
             array[i], array[j] = array[j], array[i]
+    array[left], array[i] = array[i], array[left]
+    if i > left:
+        quicksort(array, left, i - 1)
+    if right > i:
+        quicksort(array, i + 1, right)
 
 
 SORTING_METHODS = {
+    "quicksort": quicksort,
     "simple": simplest_sort,
     "Gitalev": gitalev_sort,
 }
-n = 10**3
+n = 10**3 * 5
 for name, sort in SORTING_METHODS.items():
     array = [x for x in range(1, 1 + n)]
     random.shuffle(array)
     t0 = time()
     sort(array)
     t1 = time()
-    print(f"{name:>8}: ", t1 - t0)
+    print(f"{name:>9}: ", t1 - t0)
     assert is_sorted(array)
