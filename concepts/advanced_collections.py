a1636ed712eceb141324e5b74ad81d522ac1e24e
Uladzislau Khamkou <kham@tuta.io>
2022-11-17 13:35:12 +0300

    Add Storage - container with access by IDs

--- concepts/advanced_collections.py
+++ concepts/advanced_collections.py
@@ -1,6 +1,7 @@
 # bins - idea for package name if I ever put this thing on pypy
 from collections.abc import Hashable, MutableSequence, MutableSet, Sequence, Set
 from typing import Any, Iterable, Iterator, Self, TypeVar, overload
+from random import randint
 
 T = TypeVar("T")
 
@@ -106,6 +107,10 @@ class IndexedSet(Sequence[T], Set[T]):
         step = i.step if i.step is not None else 1
         return IndexedSet(self.values[i] for i in range(start, stop, step))
 
+    def index(self, value: T, start: int = 0, stop: int = 2**32) -> int:
+        i = self.indexes.get(value, -1)
+        return i if start <= i < stop else -1
+
 
 class FrozenIndexedSet(IndexedSet[T], Hashable):
     def __hash__(self) -> int:
@@ -205,3 +210,97 @@ class MutableIndexedSet(IndexedSet[T], MutableSequence[T], MutableSet[T]):
         frozen_set.values, frozen_set.indexes = self.values, self.indexes
         self.values, self.indexes = [], {}
         return frozen_set
+
+
+U32_MAX = 2**32 - 1
+INT_TO_ID = 1777797467
+ID_TO_INT = pow(INT_TO_ID, -1, U32_MAX + 1)
+
+
+class Storage(MutableSequence[T]):
+    __slots__ = ("values", "ids", "id_generation_seed")
+
+    def __init__(self, iterable: Iterable[T] = (), seed: int | None = None):
+        if isinstance(iterable, dict) and seed is not None:
+            self.ids = MutableIndexedSet(iterable.keys())
+            self.values = list(iterable.values())
+            self.id_generation_seed = seed & U32_MAX
+            return
+        if seed is None:
+            seed = randint(0, U32_MAX)
+        else:
+            seed = (1 + seed % U32_MAX) * INT_TO_ID & U32_MAX
+        self.values = list(iterable)
+        seeds = range(seed, len(self.values) + seed)
+        self.ids = MutableIndexedSet(i * INT_TO_ID & U32_MAX for i in seeds)
+        self.id_generation_seed = seed + len(self.values)
+
+    def __len__(self) -> int:
+        return len(self.values)
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self.values)
+
+    def items(self) -> Iterator[tuple[int, T]]:
+        for i, value in enumerate(self.values):
+            yield self.ids[i], value
+
+    def insert(self, i: int, value: T) -> None:
+        if i != self.id_generation_seed * INT_TO_ID & U32_MAX:
+            raise IndexError("Random access insertion is not possible")
+        self.push(value)
+
+    def append(self, value: T) -> None:
+        self.push(value)
+
+    def push(self, value) -> int:
+        i = self.id_generation_seed * INT_TO_ID & U32_MAX
+        self.id_generation_seed += 1
+        self.values.append(value)
+        self.ids.push(i)
+        return i
+
+    def __delitem__(self, i: int | slice) -> None:
+        if not isinstance(i, int):
+            raise TypeError("Indexing <storage_object> by slice is not possible")
+        if i not in self.ids:
+            raise IndexError("Invalid ID was provided")
+        self.values[self.ids.index(i)] = self.values.pop()
+        self.ids.remove(i)
+
+    def __str__(self):
+        return "[" + " ".join(f"{i:08x}:{v!r}" for i, v in self.items()) + "]"
+
+    def __repr__(self):
+        seed = f"{self.id_generation_seed:#010x}"
+        items = ", ".join(f"{i:#010x}:{v!r}" for i, v in self.items())
+        return "Storage({" + items + "}, " + f"seed={seed})"
+
+    @overload
+    def __getitem__(self, i: int) -> T:
+        ...
+
+    @overload
+    def __getitem__(self, i: slice) -> Self:
+        ...
+
+    def __getitem__(self, i: int | slice) -> T | Self:
+        if not isinstance(i, int):
+            raise TypeError("Indexing <storage_object> by slice is not possible")
+        return self.values[self.ids.index(i)]
+
+    @overload
+    def __setitem__(self, i: int, value: T) -> None:
+        ...
+
+    @overload
+    def __setitem__(self, i: slice, iterable: Iterable[T]) -> None:
+        ...
+
+    def __setitem__(self, i: int | slice, value: Any) -> None:
+        if not isinstance(i, int):
+            raise TypeError("Indexing <storage_object> by slice is not possible")
+        self.values[self.ids.index(i)] = value
+
+    def index(self, value: T, start: int = 0, stop: int = 2**32) -> int:
+        return self.ids[self.values.index(value, start, stop)]
