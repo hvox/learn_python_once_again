f63e38c4cdbc50bdca58fe92ed0da6bdd0933be0
Uladzislau Khamkou <kham@tuta.io>
2023-05-01 14:20:50 +0300

    Put PNG decoding into a class

--- concepts/png/mypng.py
+++ concepts/png/mypng.py
@@ -1,132 +1,119 @@
-#!/usr/bin/env python3
-import zlib
-from sys import stderr, argv
+from __future__ import annotations
+
+from array import array
+from dataclasses import dataclass, field
+from itertools import count
 from pathlib import Path
-from enum import IntEnum
 from struct import unpack
+from typing import Any, Callable, Literal, NamedTuple, Self, Sequence
+from zlib import compress, crc32, decompress
+
+PNG_SIGNATURE = b"\x89PNG\r\n\x1a\n"
 
 
-class PNGError(Exception):
-    pass
+class FloatArray(array):
+    def __setitem__(self, index: Any, value: Any):
+        if isinstance(index, slice) and not isinstance(value, array):
+            value = array("f", value)
+        array.__setitem__(self, index, value)
 
 
-def error(error_msg):
-    raise PNGError("Bad " + error_msg)
+@dataclass
+class Image:
+    width: int
+    height: int
+    pixels: Sequence[float]
+
+    def __iter__(self):
+        yield from (self.width, self.height, self.pixels)
+
+    def __post_init__(self):
+        if not isinstance(self.pixels, FloatArray):
+            self.pixels = FloatArray("f", self.pixels)
 
 
-def assure(condition: bool, error_msg: str):
-    if not condition:
-        error(error_msg)
+@dataclass
+class Chunk:
+    typ: str
+    position: Literal["begin", "palette", "end"]
+    data: bytes
 
 
-class ColorType(IntEnum):
-    W = 0
-    RGB = 2
-    INDEX = 3
-    WA = 4
-    RGBA = 6
-
-    def __init__(self, number: int):
-        self.allowed_depths = {
-            0: (1, 2, 4, 8, 16),
-            2: (8, 16),
-            3: (1, 2, 4, 8),
-            4: (8, 16),
-            6: (8, 16),
-        }[number]
-        self.channels = [1, 0, 3, 1, 2, 0, 4][number]
+Palette = tuple[tuple[float, float, float, float], ...]
 
 
-def read_png(buf: bytes):
-    chunks = read_chunks(buf)
-    typ, data = next(chunks)
-    assure(typ == b"IHDR", "first chunk: it should be IHDR")
-    width, height, depth, clr_typ = read_header(data)
-    if clr_typ == ColorType.INDEX:
-        _, data = next_chunk(chunks, b"PLTE")
-        palette = read_palette(data)
-        _, data = next_chunk(chunks, b"IDAT")
-        pixels = read_pixel_data(width, height, clr_typ, depth, data)
-        for line in pixels:
-            for x, index in enumerate(line):
-                line[x] = palette[index[0]]
-    else:
-        _, data = next_chunk(chunks, b"IDAT")
-        pixels = read_pixel_data(width, height, clr_typ, depth, data)
-    next_chunk(chunks, b"IEND")
-    return pixels
+@dataclass
+class PNG:
+    image: Image
+    gamma: float | None = None
+    bitdepth: int = 16
+    interlacing: bool = False
+    colors: Literal["L", "LA", "RGB", "RGBA"] | Palette = "RGBA"
+    tags: dict[str, str] = field(default_factory=dict)
+    extra_chunks: dict[str, list[Chunk]] = field(default_factory=dict)
+
+    @staticmethod
+    def decode(data: bytes, fltr: Callable[[str], bool] | None = None) -> PNG:
+        return decode_png(data, fltr)
 
 
-def read_chunks(buf: bytes):
-    assure(buf[:8] == b"\x89PNG\r\n\x1a\n", "png signature")
-    assure(len(buf) >= 42, "file size: the file is almost empty")
-    idata_chunks = []
-    i = 8
-    while i < len(buf):
-        assure(len(buf) - i >= 12, "chunk size")
-        length, typ = unpack(">I4s", buf[i: i + 8])
-        assure(len(buf) - i >= 12 + length, f"{typ} chunk size")
-        data = buf[i + 8: i + 8 + length]
-        i += 12 + length
-        crc = unpack(">I", buf[i - 4: i])[0]
-        assure(crc == zlib.crc32(typ + data), f"{typ} chunk CRC")
-        if typ == b"IDAT" and buf[i + 4:i+8] == b"IDAT":
-            idata_chunks.append(data)
-            continue
-        if typ == b"IEND":
-            break
-        if typ == b"IDAT":
-            yield typ, b"".join(idata_chunks + [data])
-            idata_chunks.clear()
-        else:
-            yield typ, data
-    else:
-        error("chunks: there must be IEND chunk at the end")
-    assure(i == len(buf), "file end: there should be nothing after IEND chunk")
-    yield typ, data
+def decode_png(data: bytes, fltr: Callable[[str], bool] | None = None) -> PNG:
+    fltr = (lambda typ: typ[3].islower()) if fltr is None else fltr
+    chunks = split_chunks(data)
+    w, h, bitdepth, colors = decode_ihdr(chunks[0][1])
+    gama_index = find_chunk(chunks, 1, "gAMA", strict=False)
+    plte_index = find_chunk(chunks, 1, "PLTE", strict=False)
+    idat_index = find_chunk(chunks, 1, "IDAT", strict=True)
+    assure(gama_index <= plte_index < idat_index, "chunk ordering")
+    gamma = decode_gama(chunks[gama_index][1]) if gama_index != -1 else None
+    if colors is None:
+        assure(plte_index != -1, "color type")
+        colors = decode_plte(chunks[plte_index][1])
+    channels = len(colors) if isinstance(colors, str) else 1
+    pixels = decode_idat(chunks[idat_index][1], w, h, bitdepth, channels)
+    if not isinstance(colors, str):
+        pixels = sum((colors[color_index] for color_index in pixels), ())
+    image = Image(w, h, pixels)
+    png = PNG(image, gamma, bitdepth)
+    return png
 
 
-def next_chunk(chunks, desired_chunks: bytes):
-    desired_chunks_set = set(desired_chunks.split(b" "))
-    for typ, data in chunks:
-        if typ in desired_chunks_set:
-            return typ, data
-        assure(typ[0] & 1 << 5, f"chunk: {typ}. Expected {desired_chunks.decode()}")
-    error("chunk order: unexpected end of file")
-
-
-def read_header(data: bytes):
-    assure(len(data) == 13, "IHDR: it should be exactly 25 bytes long")
+def decode_ihdr(
+    data: bytes,
+) -> tuple[int, int, int, None | Literal["L", "LA", "RGB", "RGBA"]]:
     width, height, depth, clr_typ, compression, filtr, interlace = unpack(">IIBBBBB", data)
-    assure(1 <= width <= 2**31 - 1, "width: it should be between 1 and 2^31-1")
-    assure(1 <= height <= 2**31 - 1, "height: it should be between 1 and 2^31-1")
-    assure(clr_typ in (0, 2, 3, 4, 6), "color type")
-    clr_typ = ColorType(clr_typ)
-    assure(depth in clr_typ.allowed_depths, "bit depth")
-    assure(compression == 0, "compression method: it should be 0")
-    assure(filtr == 0, "filter method: it should be 0")
-    # TODO: support Adam7 interlace
+    assure(1 <= width <= 2**31 - 1 and 1 <= height <= 2**31 - 1, "image size")
+    assure(compression == 0 and filtr == 0 and interlace in (0, 1), "IHDR last bytes")
     assert interlace == 0, "TODO: support Adam7 interlace"
-    return (width, height, depth, clr_typ)
+    color_type_is_ok = (  # TODO: Use any(), when mypy is fixed
+        clr_typ == 0 and depth in (1, 2, 4, 8, 16),
+        clr_typ in (2, 4, 6) and depth in (8, 16),
+        clr_typ == 3 and depth in (1, 2, 4, 8),
+    )
+    assure(any(color_type_is_ok), "image color type")
+    colors = ["L", None, "RGB", None, "LA", None, "RGBA"][clr_typ]
+    return (width, height, depth, colors)
 
 
-def read_palette(data: bytes):
-    assure(len(data) % 3 == 0, "palette: it should have integer number of colors")
-    return [data[i: i + 3] for i in range(0, len(data), 3)]
+def decode_gama(data: bytes) -> float:
+    gamma = unpack(">I", data)[0] / 100000
+    return gamma
 
 
-def read_pixel_data(width: int, height: int, clr_typ: int, depth: int, data: bytes):
-    # TODO errors?
-    data = zlib.decompress(data)
-    bits_per_pixel = depth * clr_typ.channels
+def decode_plte(data: bytes) -> Palette:
+    assure(len(data) % 3 == 0, "PLTE size")
+    return tuple((r / 255, g / 255, b / 255, 1.0) for r, g, b in (data[i: i + 3] for i in range(0, len(data), 3)))
+
+
+def decode_idat(data: bytes, width: int, height: int, bitdepth: int, channels: int) -> array:
+    # TODO: raise PNGError on errors?
+    data = decompress(data)
+    bits_per_pixel = bitdepth * channels
     d = bytes_per_pixel = (bits_per_pixel + 7) // 8
     line_length = (bits_per_pixel * width + 7) // 8 + 1
     fltrs = [data[i] for i in range(0, len(data), line_length)]
     lines = [bytearray(data[i + 1: i + line_length]) for i in range(0, len(data), line_length)]
     for y, (fltr, line) in enumerate(zip(fltrs, lines)):
-        print(f"{y}:f{fltr} " + " ".join(
-                line[i: i + bytes_per_pixel].hex() for i in range(0, len(line), bytes_per_pixel)
-        ))
         assert 0 <= fltr <= 4
         if fltr == 0:  # None
             pass
@@ -148,8 +135,11 @@ def read_pixel_data(width: int, height: int, clr_typ: int, depth: int, data: byt
                 upleft = lines[y - 1][x - d] if y > 0 and x >= d else 0
                 p = left + up - upleft
                 peach = (
-                    left if abs(p - up) >= abs(p - left) <= abs(p - upleft) else
-                    up if abs(p - up) <= abs(p - upleft) else upleft
+                    left
+                    if abs(p - up) >= abs(p - left) <= abs(p - upleft)
+                    else up
+                    if abs(p - up) <= abs(p - upleft)
+                    else upleft
                 )
                 line[x] = (line[x] + peach) % 256
     if bits_per_pixel < 8:
@@ -161,15 +151,52 @@ def read_pixel_data(width: int, height: int, clr_typ: int, depth: int, data: byt
             for line in lines
         ]
     pixels = [
-        [line[x * bytes_per_pixel: x * bytes_per_pixel + bytes_per_pixel] for x in range(width)]
-        for line in lines
+        [line[x * bytes_per_pixel: x * bytes_per_pixel + bytes_per_pixel] for x in range(width)] for line in lines
     ]
-    return pixels
+    return b"".join(sum(pixels, []))
 
 
-for png_path in argv[1:]:
-    if len(argv[1:]) > 1:
-        print("\n> " + png_path)
-    for line in read_png(Path(png_path).read_bytes()):
-        s = " ".join(pixel.hex() for pixel in line)
-        print(s if len(s) < 160 else s[:157] + "...")
+def split_chunks(bytes: bytes) -> list[tuple[str, bytes]]:
+    assure(bytes[:8] == PNG_SIGNATURE, "PNG Signature")
+    i, chunks = 8, []
+    while i < len(bytes):
+        assure(len(bytes) - i >= 12, "Chunk size")
+        length, typ = unpack(">I4s", bytes[i: i + 8])
+        assure(is_correct_chunk_type(typ), "chunk type")
+        assure(len(bytes) - i >= 12 + length, f"{typ} chunk size")
+        data = bytes[i + 8: i + 8 + length]
+        i += 12 + length
+        crc = unpack(">I", bytes[i - 4: i])[0]
+        assure(crc == crc32(typ + data), f"{typ} chunk CRC")
+        chunks.append((typ.decode(), data))
+    assure(len(chunks) > 0 and chunks[0][0] == "IHDR", "IHDR position")
+    assure(chunks[-1][0] == "IEND", "IEND position")
+    assure(len(chunks[-1][1]) == 0, "IEND content")
+    return chunks
+
+
+def is_correct_chunk_type(typ: str | bytes):
+    if isinstance(typ, str):
+        typ = typ.encode("utf-8")
+    return len(typ) == 4 and typ.isalpha() and typ[2] & 5
+
+
+def find_chunk(chunks: list[tuple[str, bytes]], i: int, typ: str, strict=False):
+    while i < len(chunks):
+        if chunks[i][0] == typ:
+            return i
+        i += 1
+    if strict:
+        raise PNGError(f"{typ} chunk not found")
+    return -1
+
+
+class PNGError(Exception):
+    @staticmethod
+    def blame(error_location: str):
+        raise PNGError(f"Incorrect {error_location}")
+
+
+def assure(condition: bool, error_location: str):
+    if not condition:
+        raise PNGError.blame(error_location)
