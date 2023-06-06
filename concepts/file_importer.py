4db37383018803e6f4ade9d6171776ee336079f6
Uladzislau Khamkou <kham@tuta.io>
2023-06-06 23:57:14 +0300

    Finish the file_importer
    Perfection is achieved, not when there is nothing more to add,
    but when there is nothing left to take away.

--- concepts/file_importer.py
+++ concepts/file_importer.py
@@ -1,68 +1,57 @@
-from collections import namedtuple
+import re
+from functools import reduce
 from inspect import currentframe
 from pathlib import Path
-from typing import Any, Callable, Generic, TypeVar, Type
+from typing import Any, Type, TypeVar
 
+__all__ = ["load"]
 T = TypeVar("T")
+CAMELCASE_REGEX = re.compile("((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))")
 
 
-class Files(Generic[T]):
-    def __init__(self, loader: Callable[[Path], T]):
-        self.load = loader
-        self.cache: dict[Path, T] = {}
-
-    def __getitem__(self, local_path: str) -> T:
-        frame: Any = currentframe()
-        root = Path(frame.f_back.f_globals["__file__"]).resolve().parent
-        path = root.joinpath(*local_path.split("/"))
-        while not path.exists() and path not in self.cache and root.parent != root:
-            root = root.parent
-            path = root.joinpath(*local_path.split("/"))
-        if path in self.cache:
-            return self.cache[path]
-        if not path.exists():
-            raise FileNotFoundError(f"???/{local_path}")
-        self.cache[path] = self.load(path)
-        return self.cache[path]
-
-    def __setitem__(self, local_path: str, data: T):
-        frame: Any = currentframe()
-        root = Path(frame.f_back.f_globals["__file__"]).resolve().parent
-        path = root.joinpath(*local_path.split("/"))
-        while not path.exists() and path not in self.cache and root.parent != root:
-            root = root.parent
-            path = root.joinpath(*local_path.split("/"))
-        self.cache[path] = data
+def load(typ: Type[T], relative_path: str | None = None) -> T:
+    if relative_path is None:
+        relative_path = guess_path(typ)
+    path = resolve(relative_path)
+    lines = path.read_text().strip("\n").split("\n")
+    header, *rows = (list(map(unwrap, line.split("\t"))) for line in lines)
+    fields = {h.lower(): i for i, h in enumerate(header)}
+    t: Any = typ
+    assert t.__origin__ is list
+    t = t.__args__[0]
+    result: Any = []
+    for row in rows:
+        attributes: dict[str, Any] = {}
+        for field, field_type in t.__annotations__.items():
+            attributes[field] = field_type(row[fields[field]])
+        result.append(t(**attributes))
+    return result
 
 
-def read_bytes(path: Path) -> bytes:
-    return path.read_bytes()
+def camel_to_snake(text: str) -> str:
+    return CAMELCASE_REGEX.sub(r"_\1", text).lower()
 
 
-def read_text(path: Path) -> str:
-    return read_bytes(path).decode("utf-8")
+def pluralize(text: str) -> str:
+    return text + "s"
 
 
-def read_lines(path: Path) -> list[str]:
-    return read_text(path).removesuffix("\n").split("\n")
+def guess_path(t: type) -> str:
+    name = camel_to_snake(getattr(t, "__args__", (t,))[0].__name__)
+    return pluralize(name.replace("_", " ")).replace(" ", "-") + ".tsv"
 
 
-def read_grid(path: Path) -> list[list[str]]:
-    return [list(map(unwrap, line.split("\t"))) for line in read_lines(path)]
-
-
-def read_table(path: Path) -> list[tuple[str, ...]]:
-    header, *rows = read_grid(path)
-    fields = [field.lower().replace(" ", "_") for field in header]
-    RowType = namedtuple("", fields)  # type: ignore
-    return [RowType(*row) for row in rows]
-
-
-def read_dict(path: Path) -> dict[str, tuple[str, ...]]:
-    (_, *header), *rows = read_grid(path)
-    fields = [field.lower().replace(" ", "_") for field in header]
-    ValueType = namedtuple("", fields)  # type: ignore
-    return {key: ValueType(*info) for key, *info in rows}
+def resolve(relative_path: str, callee_lvl: int = 0) -> Path:
+    frame: Any = currentframe()
+    frame = reduce(lambda x, _: x.f_back, range(callee_lvl + 1), frame)
+    root = Path(frame.f_globals["__file__"]).resolve().parent
+    path = root.joinpath(*relative_path.split("/"))
+    while not path.exists() and root.parent != root:
+        root = root.parent
+        path = root.joinpath(*relative_path.split("/"))
+    if not path.exists():
+        raise FileNotFoundError(f"???/{relative_path}")
+    return path
 
 
 def unwrap(source: str) -> str:
@@ -76,11 +65,3 @@ def unwrap(source: str) -> str:
             cell_value.append(source[i])
             i += 1
     return "".join(cell_value)
-
-
-BINARIES: Files[bytes] = Files(read_bytes)
-TEXTS: Files[str] = Files(read_text)
-LISTS: Files[list[str]] = Files(read_lines)
-GRIDS: Files[list[list[str]]] = Files(read_grid)
-TABLES: Files[list[tuple[str, ...]]] = Files(read_table)
-DICTS: Files[dict[str, tuple[str, ...]]] = Files(read_dict)
