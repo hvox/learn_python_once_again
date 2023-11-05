b30870ffc111648b8e9019d80f560273509f1ea0
Uladzislau Khamkou <kham@tuta.io>
2023-11-05 23:03:33 +0300

    Add exec_cmd(command)

---
+++ my_personal_snippet_library/exec_cmd.py
@@ -0,0 +1,12 @@
+import subprocess
+
+
+def exec_cmd(cmd: str | list, into: str | bytes | None = None, **kwargs):
+    check = kwargs.pop("check", True)
+    inp = into.encode("utf-8") if isinstance(into, str) else into
+    args = list(cmd.split(" ") if isinstance(cmd, str) else map(str, cmd))
+    return subprocess.run(args, check=check, input=inp, **kwargs)
+
+
+if __name__ == "__main__":
+    exec_cmd(f"batcat {__file__}")
