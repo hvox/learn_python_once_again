bcdfb8379a29786939a481a820c778df8d5381aa
Uladzislau Khamkou <kham@tuta.io>
2023-12-14 23:42:49 +0300

    Rename quote(str) to quoted(str)

--- my_personal_snippet_library/quoted.py
+++ my_personal_snippet_library/quoted.py
@@ -1,8 +1,8 @@
-def quote(string: str):
+def quoted(string: str):
     table = {'"': '\\"', "\\": "\\\\", "\n": "\\n", "\r": "\\r", "\t": "\\t"}
     return '"' + string.translate(str.maketrans(table)) + '"'
 
 
 if __name__ == "__main__":
     for s in "aboba dbl\" sngl' 2'\"'".split():
-        print(quote(s))
+        print(quoted(s))
