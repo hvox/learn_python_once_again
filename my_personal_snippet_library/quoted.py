86caab8bdc55e25708b75cdc29228762d6a9ee85
Uladzislau Khamkou <kham@tuta.io>
2024-01-01 23:40:06 +0300

    Support non-double quotes

--- my_personal_snippet_library/quoted.py
+++ my_personal_snippet_library/quoted.py
@@ -1,6 +1,8 @@
 def quoted(string: str):
-    table = {'"': '\\"', "\\": "\\\\", "\n": "\\n", "\r": "\\r", "\t": "\\t"}
-    return '"' + string.translate(str.maketrans(table)) + '"'
+    table = {"\\": "\\\\", "\n": "\\n", "\r": "\\r", "\t": "\\t"}
+    quotes = '"' if "'" in string or '"' not in string else "'"
+    trans = str.maketrans(table | {quotes: "\\" + quotes})
+    return quotes + string.translate(trans) + quotes
 
 
 if __name__ == "__main__":
